4af51c78aae8e5ef5d30595938f387c4f4d2e1ad
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-09 00:18:33 -0500

    add code to satisfy triangle.test.js

--- src/index.js
+++ src/index.js
@@ -6,19 +6,13 @@ import Triangle from './triangle.js';
 
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) 
-{
-  /* eslint-disable */
+  $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
-    debugger;
     const length1 = $('#length1').val();
     const length2 = $('#length2').val();
     const length3 = $('#length3').val();
     const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType(); dinosaur
-    debugger;
     $('#response').append("<p>" + response + "</p>");
-    debugger;
   });
 });
-/* eslint-enable */
